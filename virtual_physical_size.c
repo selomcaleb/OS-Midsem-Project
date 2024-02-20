e0d862aa1cf8e01460171cb16268bab691b78a27
vboxuser <vboxuser@ChuUbuntu.myguest.virtualbox.org>
2024-02-20 16:34:15 +0000

    Defined the sizes

---
+++ virtual_physical_size.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+// defined in bytes
+
+#define PHYSICAL_MEMORY_SIZE 1024
+#define VIRTUAL_MEMORY_SIZE 4096
+
+/**
+int page_faults = 0;
+
+int num_frames = 0; //supposed to equal NUM_FRAMES
+
+int allocated_frames = 0;
+void handlePageFault(int page_number){
+	page_faults+=1;
+	int frame_number = rand()%NUM_FRAMES;
+	page_table[page_number] = frame_number;
+	page_table[page_number].valid = 1;
+
+	printf("Loading data from secondary storage for page %d to frame %d in memory", page_number, frame_number );
+
+}
+
+int allocate_frame(){
+
+}
+**/
+int main(){
+return 0;
+
+}
