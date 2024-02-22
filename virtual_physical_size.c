55cec1ee7197e73cbf22edc02e7bbcc4ede7e609
selomcaleb <selomcaleb@gmail.com>
2024-02-22 13:02:48 +0000

    function for page allocation

--- virtual_physical_size.c
+++ virtual_physical_size.c
@@ -26,6 +26,23 @@ typedef struct {
 PhysicalMemoryFrame physical_memory[PHYSICAL_MEMORY_SIZE];
 
 
+//function for page allocation
+void allocate_page(int page_number){
+    int frame_number = allocate_frame();
+    if (frame_number == -1) {
+        printf("Failed to allocate a frame for page %d due to lack of available frames.\n", page_number);
+        return; 
+    }
+    page_table[page_number].frame_number = frame_number;
+    page_table[page_number].valid = 1;
+}
+
+//function for page deallocation
+void deallocate_page(int page_number){
+
+}
+
+
 int page_faults = 0;
 
 int num_frames = 0; //supposed to equal NUM_FRAMES
@@ -34,7 +51,7 @@ int allocated_frames = 0;
 void handlePageFault(int page_number){
 	page_faults+=1;
 	int frame_number = rand()%NUM_FRAMES;
-	page_table[page_number] = frame_number;
+	page_table[page_number].frame_number = frame_number;
 	page_table[page_number].valid = 1;
 	printf("Loading data from secondary storage for page %d to frame %d in memory", page_number, frame_number );
 }
