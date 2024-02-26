909ff4017958199019e11ba0ba89428fe96d0e5e
Oheneba Kwaku Addo Dade <ohenebadade@gmail.com>
2024-02-26 13:33:47 +0000

    redo the translate address function

--- virtual_physical_size.c
+++ virtual_physical_size.c
@@ -31,33 +31,14 @@ int page_faults = 0;
 int num_frames = 0; //supposed to equal NUM_FRAMES
 
 int allocated_frames = 0;
-void handlePageFault(int page_number){
-	page_faults+=1;
-	int frame_number = rand()%NUM_FRAMES;
-	page_table[page_number] = frame_number;
-	page_table[page_number].valid = 1;
-	printf("Loading data from secondary storage for page %d to frame %d in memory", page_number, frame_number );
-}
+//void handlePageFault(int page_number){
+//	page_faults+=1;
+//	int frame_number = rand()%NUM_FRAMES;
+//	page_table[page_number] = frame_number;
+//	page_table[page_number].valid = 1;
+//	printf("Loading data from secondary storage for page %d to frame %d in memory", page_number, frame_number );
+//}
 
 
-int translateAddress(int virtual_address) {
-    int page_number = virtual_address / PHYSICAL_MEMORY_SIZE;
-    int offset = virtual_address % PHYSICAL_MEMORY_SIZE;
 
-    if (page_table[page_number].valid) {
-        int frame_number = page_table[page_number].frame_number;
-        int physical_address = frame_number * PHYSICAL_MEMORY_SIZE + offset;
-        return physical_address;
-    } else {
-        handlePageFault(page_number);
-    }
-}
 
-int allocate_frame(){
-
-}
-
-int main(){
-return 0;
-
-}
