520106c202de67f10fb1435f669e0495ddeb5321
chudah1 <yakung25c@gmail.com>
2024-02-26 18:03:17 +0000

    added comments

--- frames.c
+++ frames.c
@@ -9,11 +9,28 @@ typedef struct {
     int pageNumber;
 } FrameEntry;
 
+/**
+ * The above type is a struct representing a frame table with a specified capacity and an array of
+ * frame entries.
+ * @property {int} capacity - The `capacity` property in the `FrameTable` struct represents the maximum
+ * number of frames that can be stored in the frame table. It indicates the total number of frames that
+ * the frame table can hold at any given time.
+ * @property {FrameEntry} frames - The `frames` property in the `FrameTable` struct is an array of
+ * `FrameEntry` elements. Each `FrameEntry` element represents a frame in the frame table.
+ */
 typedef struct {
     int capacity;
     FrameEntry frames[NUM_FRAMES];
 } FrameTable;
 
+/**
+ * The function initializes a frame table by setting the frame numbers and page numbers for each frame.
+ * 
+ * @param frameTable The `frameTable` parameter is a pointer to a `FrameTable` struct. The `FrameTable`
+ * struct likely contains information about the frame table used in a system, such as the capacity of
+ * the frame table and an array of frames. The function `initializeFrameTable` initializes the frame
+ * table
+ */
 void initializeFrameTable(FrameTable* frameTable) {
     frameTable->capacity = NUM_FRAMES;
     int capacity = frameTable->capacity;
@@ -25,6 +42,16 @@ void initializeFrameTable(FrameTable* frameTable) {
     }
 }
 
+/**
+ * This function calculates the number of free frames in a given frame table.
+ * 
+ * @param frameTable FrameTable* frameTable is a pointer to a structure that represents a table of
+ * frames in memory. Each frame in the table contains information about a page, such as the page number
+ * currently stored in the frame.
+ * 
+ * @return The function `trackNumberFreeFrames` returns the number of free frames in the `FrameTable`
+ * structure.
+ */
 int trackNumberFreeFrames(FrameTable* frameTable) {
     int freeCount = 0;
     for (int i = 0; i < frameTable->capacity; i++) {
@@ -34,6 +61,18 @@ int trackNumberFreeFrames(FrameTable* frameTable) {
     }
     return freeCount;
 }
+/**
+ * The function `trackNumberAllocatedFrames` counts the number of allocated frames in a given
+ * `FrameTable`.
+ * 
+ * @param frameTable The `frameTable` parameter is a pointer to a `FrameTable` struct. The `FrameTable`
+ * struct likely contains information about a set of frames, such as an array of frames or other
+ * relevant data related to memory management. The function `trackNumberAllocatedFrames` iterates
+ * through the
+ * 
+ * @return The function `trackNumberAllocatedFrames` returns the count of allocated frames in the
+ * `frameTable`.
+ */
 
 int trackNumberAllocatedFrames(FrameTable* frameTable){
 	int allocatedCount = 0;
@@ -46,12 +85,24 @@ int trackNumberAllocatedFrames(FrameTable* frameTable){
 	return allocatedCount;
 }
 
+/**
+ * The function checks if a frame in a frame table is allocated.
+ * 
+ * @param frameTable FrameTable is a data structure that keeps track of the status of each frame in the
+ * memory. It typically contains information such as the page number currently stored in each frame.
+ * @param frame The `frame` parameter represents the index of the frame in the frame table that we want
+ * to check if it is allocated or not.
+ * 
+ * @return The function `frameIsAllocated` returns 1 if the frame is allocated (i.e., the page number
+ * is not -1 in the frame table), and it returns 0 if the frame is not allocated.
+ */
 int frameIsAllocated(FrameTable* frameTable, int frame){
 	if (frameTable->frames[frame].pageNumber!=-1){
 		return 0;
 	}
 	return 1;
 }
+
 int main() {
     FrameTable frameTable;
     initializeFrameTable(&frameTable);
