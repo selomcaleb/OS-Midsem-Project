f4433d1c693ed7317df0bfb41d7d150e8e7fe9f5
Oheneba Kwaku Addo Dade <ohenebadade@gmail.com>
2024-02-27 13:38:57 +0000

    include documentation for util function

--- utils/extract_address_bits.c
+++ utils/extract_address_bits.c
@@ -1,3 +1,15 @@
+/**
+ * The function `extractAddressBits` takes a virtual address as input, extracts the outer index, inner
+ * index, and offset from the address, and returns them as an array of integers.
+ * 
+ * @param virtual_address `virtual_address` is an unsigned short integer that represents a virtual
+ * memory address. The function `extractAddressBits` takes this virtual address as input and extracts
+ * specific parts of it to return as an array of integers. Virtual Addresses in this simulation are 16 bit addresses.
+ * 
+ * @return The function `extractAddressBits` is returning a pointer to an array of integers that
+ * contains the extracted values of the outer index, inner index, and offset from the given virtual
+ * address.
+ */
 int* extractAddressBits(unsigned short virtual_address) {
     // Create an array to store the extracted values
     static int result[3];
