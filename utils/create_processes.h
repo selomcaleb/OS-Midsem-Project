65c3ff089de283f7c9ee60b0abd12ffce0986f85
Oheneba Kwaku Addo Dade <ohenebadade@gmail.com>
2024-02-29 22:59:47 +0000

    add create processes function

---
+++ utils/create_processes.h
@@ -0,0 +1,7 @@
+#ifndef OS_MIDSEM_PROJECT_CREATE_PROCESSES_H
+#define OS_MIDSEM_PROJECT_CREATE_PROCESSES_H
+
+#include "process.h"
+Process* createProcesses(int numProcesses);
+
+#endif //OS_MIDSEM_PROJECT_CREATE_PROCESSES_H
