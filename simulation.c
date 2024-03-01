f6bb2d0803168dc25b44c6d818eb3e945461ad0d
Oheneba Kwaku Addo Dade <ohenebadade@gmail.com>
2024-03-01 02:32:34 +0000

    initial sim

--- simulation.c
+++ simulation.c
@@ -1,3 +1,17 @@
-//
-// Created by Oheneba Dade on 01/03/2024.
-//
+#include "utils/create_page_table.h"
+#include "utils/create_processes.h"
+#include "utils/page_table_entry.h"
+#include "utils/process.h"
+#include <stdio.h>
+
+int main() {
+    // Create the master page table
+    PageTableEntry** masterPageTable = createMasterPageTable();
+    printf("Master page table created\n");
+
+    // create the processes
+    Process* processes = createProcesses(3);
+    printf("Processes created\n");
+
+
+}
